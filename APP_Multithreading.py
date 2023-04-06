1bd1324fe507ed08885e2ac92c920923a2005dae
CHAITANYA SHARMA <94222140+chaitanyaa1512@users.noreply.github.com>
2023-04-05 23:30:41 -0700

    Add files via upload

---
+++ APP_Multithreading.py
@@ -0,0 +1,44 @@
+import threading
+print("Hello World!", threading.current_thread().getName())
+
+#thread creation without using any class
+import threading
+import time
+def activate(): #user defined method
+   for i in range(1,11):
+     time.sleep(2)
+     print("This is a Child Thread\n")
+t =threading.Thread (target=activate) #creating thread object
+t.start()#starting of thread
+
+for i in range(1,11):
+  time.sleep(2)
+  print("This is: Main thread",threading.current_thread().getName())
+
+#thread creation with extension of thread class(Inheritance)
+from threading import*
+import time
+class MyThread(Thread): #child class
+   def run(self):
+    for i in range(10):
+      time.sleep(2)
+      print("Child Thread-1")
+obj_t=MyThread()
+obj_t.start()
+for i in range(10):
+  time.sleep(2)
+  print("Main Thread-1")
+
+#thread creation without extending a thread class
+from threading import*
+import time
+class MyTest:
+  def display(self):
+    for i in range(10):
+      print("Child Thread\n")
+obj=MyTest()
+t=threading.Thread(target=obj.display)
+t.start()
+
+for i in range(10):
+  print("Main Thread\n")
